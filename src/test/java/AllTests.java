5808dfa09684cf3a55d4606b573dbd967d5d6778
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2023-09-18 13:48:14 +0200

    trying to get tests to automatically run on commit

---
+++ src/test/java/AllTests.java
@@ -0,0 +1,10 @@
+
+import org.junit.runner.RunWith;
+import org.rascalmpl.test.infrastructure.RascalJUnitTestPrefix;
+import org.rascalmpl.test.infrastructure.RascalJUnitTestRunner;
+
+@RunWith(RascalJUnitTestRunner.class)
+@RascalJUnitTestPrefix("lang::php")
+public class AllTests {
+
+}
